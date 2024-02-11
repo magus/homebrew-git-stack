f8cef516a58b61ff5066158dd1eec35e70d2aac5
magus <noah@iamnoah.com>
2024-02-11 05:08:53 -0800

    1.0.0

--- git-stack.rb
+++ git-stack.rb
@@ -1,14 +1,14 @@
 class GitStack < Formula
   desc "stacked diffs for git"
   homepage "https://github.com/magus/git-stack-cli"
-  version "0.8.8"
+  version "1.0.0"
 
   if OS.mac?
-    url "https://github.com/magus/git-stack-cli/releases/download/0.8.8/git-stack-cli-macos"
-    sha256 "34042b4886fbbc651132d6e6c33daa4ca072d730bf156b91f03db31317c0a5d9"
+    url "https://github.com/magus/git-stack-cli/releases/download/1.0.0/git-stack-cli-macos"
+    sha256 "3d2ba5ea55e6644e55d88c20bfbad9783676122225b2d7722c8146425b6be740"
   elsif OS.linux?
-    url "https://github.com/magus/git-stack-cli/releases/download/0.8.8/git-stack-cli-linux"
-    sha256 "6aacee2397849645c59bbd390ac3390f756778a2ff9858ca96ad265bc1cd2052"
+    url "https://github.com/magus/git-stack-cli/releases/download/1.0.0/git-stack-cli-linux"
+    sha256 "0494501d53b1103d1de76da6a12238d0964f9751efb98eaa65ec63e10d515557"
   end
 
   def install
